0272d6bb3a6229e00063d495711ca788ee953f45
ro-mgh <romyagchenkov@gmail.com>
2021-02-13 19:44:16 +0700

    Dockerfile config

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:12-alpine
+
+RUN npm i -g nodemon
+
+USER node
+
+RUN mkdir /home/node/code
+
+WORKDIR /home/node/code
+
+COPY --chown=node:node package-lock.json package.json ./
+
+RUN npm ci
+
+COPY --chown=node:node . .
+
+CMD ["nodemon", "src/index.ts"]
--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -1,9 +1,19 @@
 var admin = require("firebase-admin");
-
-// export GOOGLE_APPLICATION_CREDENTIALS="/Users/rmyagchenkov/Desktop/Insta project/fb-instaapp-firebase-adminsdk-r9x6t-437faf32c2.json"
+require("dotenv").config();
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    type: process.env.FB_ADMIN_TYPE,
+    project_id: process.env.FB_ADMIN_PROJECT_ID,
+    private_key_id: process.env.FB_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.FB_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FB_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.FB_ADMIN_CLIENT_ID,
+    auth_uri: process.env.FB_ADMIN_AUTH_URI,
+    token_uri: process.env.FB_ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_ADMIN_AUTH_PROVIDER,
+    client_x509_cert_url: process.env.FB_ADMIN_CLIENT_CERT,
+  }),
 });
 
 export default admin;
