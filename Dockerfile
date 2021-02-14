8d71cf2257eca0b4081b982479ac21e013a5bc9f
ro-mgh <romyagchenkov@gmail.com>
2021-02-14 20:13:05 +0700

    Dockerfile with prisma

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,51 @@
 
-FROM node:12-alpine
+# FROM node:12-alpine
+
+# # Create app directory
+# USER node
+# RUN mkdir /home/node/code
+# WORKDIR /home/node/code
+
+# # Install app dependencies
+# COPY --chown=node:node package-lock.json package.json ./
+
+# RUN npm ci
+
+# # Bundle app source
+# COPY . /home/node/code
+# RUN npm run build
+
+# EXPOSE 3000
+# CMD [ "npm", "start" ]
+
+
+
+# test 2
+
+FROM node:12-alpine AS builder
 
 # Create app directory
-USER node
-RUN mkdir /home/node/code
-WORKDIR /home/node/code
+WORKDIR /app
+
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
+COPY prisma ./prisma/
 
 # Install app dependencies
-COPY --chown=node:node package-lock.json package.json ./
-RUN npm ci
+RUN npm install
+# Generate prisma client, leave out if generating in `postinstall` script
+RUN npx prisma generate
 
+COPY . .
 
-# Bundle app source
-COPY . /home/node/code
 RUN npm run build
 
-EXPOSE 3000
-CMD [ "npm", "start" ]
+FROM node:12-alpine
 
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 3000
+
+CMD [ "npm", "run", "start:prod" ]
--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -1,19 +1,20 @@
 var admin = require("firebase-admin");
 require("dotenv").config();
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FB_ADMIN_TYPE,
-    project_id: process.env.FB_ADMIN_PROJECT_ID,
-    private_key_id: process.env.FB_ADMIN_PRIVATE_KEY_ID,
-    private_key: process.env.FB_ADMIN_PRIVATE_KEY,
-    client_email: process.env.FB_ADMIN_CLIENT_EMAIL,
-    client_id: process.env.FB_ADMIN_CLIENT_ID,
-    auth_uri: process.env.FB_ADMIN_AUTH_URI,
-    token_uri: process.env.FB_ADMIN_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FB_ADMIN_AUTH_PROVIDER,
-    client_x509_cert_url: process.env.FB_ADMIN_CLIENT_CERT,
-  }),
-});
+if (process.env.FB_ADMIN_PRIVATE_KEY) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.FB_ADMIN_TYPE,
+      project_id: process.env.FB_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FB_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FB_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FB_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FB_ADMIN_CLIENT_ID,
+      auth_uri: process.env.FB_ADMIN_AUTH_URI,
+      token_uri: process.env.FB_ADMIN_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FB_ADMIN_AUTH_PROVIDER,
+      client_x509_cert_url: process.env.FB_ADMIN_CLIENT_CERT,
+    }),
+  });
+}
 
 export default admin;
